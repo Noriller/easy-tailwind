9ebfc8e74e9f02e08f9be8c3f89378cd64557c8a
Bruno <brunonoriller@gmail.com>
2022-12-13 18:51:19 -0300

    feat: adding react transformer

---
+++ src/transform/react/index.ts
@@ -0,0 +1,8 @@
+import { baseReplacer } from '../baseReplacer';
+
+/**
+ * This regex matches the syntax of class names and returns the $1 group as the args of the EasyTailwind function.
+ */
+const findClasses = /class(?:name)?={\W*?(?:e|etw)\((.*?)\)\W*?}/gis;
+
+export const replacer = baseReplacer(findClasses);
