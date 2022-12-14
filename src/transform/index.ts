b64b24af2b6db2aa8847fff3bf600b19efdbdf3c
Bruno <brunonoriller@gmail.com>
2022-12-14 10:25:01 -0300

    feat: adding a catchall replacer

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -1,1 +1,9 @@
-export * from './baseReplacer';
+import { baseReplacer } from './baseReplacer';
+
+/**
+ * This regex matches anything that resembles EasyTW and returns the $1 group as the args of the EasyTailwind function.
+ */
+const findClasses = /[\W|\(|\[|\{\S]*?(?:e|etw)\((.*?)\)[\W|)|\]|}\S]*?/gis;
+
+export const replacer = baseReplacer(findClasses);
+
