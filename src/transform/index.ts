3b94944c2545e60b48d4f839300e5ddd82da19b2
Bruno <brunonoriller@gmail.com>
2022-12-14 10:25:26 -0300

    feat: exporting the content for the catch all and basereplacer in the index

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -7,3 +7,11 @@ const findClasses = /[\W|\(|\[|\{\S]*?(?:e|etw)\((.*?)\)[\W|)|\]|}\S]*?/gis;
 
 export const replacer = baseReplacer(findClasses);
 
+const content = {
+  files: ['!node_modules', './**/*.{js,ts,jsx,tsx,html,vue,svelte,astro}'],
+  transform: {
+    DEFAULT: replacer,
+  },
+};
+
+export { baseReplacer, content };
